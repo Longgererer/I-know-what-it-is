aa36b04a783f7e41b95b33e788b8f1ce3ac379ca
xiaolong Hao <2072451919@qq.com>
2021-05-16 15:30:38 +0800

    创建Board组件并添加若干配置项参数

---
+++ src/components/Board.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="Board" ref="board">
+    <canvas
+      ref="canvas"
+      class="canvas"
+      :class="cursorType === 'default' && 'cursor-default'"
+      @mousemove="onMouseMove"
+      @mousedown="onMouseDown"
+      @mouseup="onMouseUp"
+      @mouseleave="onMouseLeave"
+    ></canvas>
+    <div class="cursor circle" :class="`cursor-${cursorType}`" :style="cursorStyle"></div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, reactive, ref, Ref, computed, ComputedRef } from 'vue'
+export default defineComponent({
+  props: {
+    penColor: {
+      type: String,
+      default: '#000000',
+    },
+    lineWidth: {
+      type: Number,
+      default: 8,
+    },
+    cursorType: {
+      type: String,
+      default: 'default', // default|pen
+    },
+  },
+  setup(props) {
+    const board: Ref<null | HTMLElement> = ref(null)
+    const canvas: Ref<null | HTMLElement> = ref(null)
+    const canvasSize = reactive({ width: 0, height: 0 })
+    const ctx: Ref<null | CanvasRenderingContext2D> = ref(null)
+    const isDrawing: Ref<boolean> = ref(false)
+    const starPosition = reactive({ x: 0, y: 0 })
+    const init = (ctx: CanvasRenderingContext2D) => {
+      const { width, height } = canvasSize
+      ctx.beginPath()
+      ctx.rect(0, 0, width, height)
+      ctx.fillStyle = '#F2F2F2'
+      ctx.fill()
+    }
+    const draw = (event: MouseEvent, ctx: CanvasRenderingContext2D) => {
+      ctx.lineJoin = 'round'
+      ctx.lineCap = 'round'
+      ctx.lineWidth = props.lineWidth
+      ctx.strokeStyle = props.penColor
+      ctx.beginPath()
+      const { x, y } = starPosition
+      ctx.moveTo(x, y)
+      const { offsetX, offsetY } = event
+      ctx.lineTo(offsetX, offsetY)
+      ctx.stroke()
+      Object.assign(starPosition, { x: offsetX, y: offsetY })
+    }
+    const clearCanvas = () => {
+      init(ctx.value as CanvasRenderingContext2D)
+    }
+    const onMouseMove = (event: MouseEvent) => {
+      if (isDrawing.value) draw(event, ctx.value as CanvasRenderingContext2D)
+      const { offsetX: x, offsetY: y } = event
+      Object.assign(starPosition, { x, y })
+    }
+    const onMouseDown = () => {
+      isDrawing.value = true
+    }
+    const onMouseUp = () => {
+      isDrawing.value = false
+    }
+    const onMouseLeave = () => {
+      isDrawing.value = false
+    }
+
+    // lifecycle
+    onMounted(() => {
+      const { clientWidth: width, clientHeight: height } = <HTMLElement>board.value
+      ctx.value = <CanvasRenderingContext2D>(canvas.value as HTMLCanvasElement).getContext('2d')
+      Object.assign(ctx.value.canvas, { width, height })
+      Object.assign(canvasSize, { width, height })
+      init(ctx.value)
+    })
+    // computed
+    const cursorStyle: ComputedRef<Object> = computed(() => {
+      const { lineWidth, penColor } = props
+      const { x, y } = starPosition
+      return {
+        backgroundColor: penColor,
+        width: `${lineWidth}px`,
+        height: `${lineWidth}px`,
+        transform: `translate(${x - lineWidth / 2}px,${y - lineWidth / 2}px)`,
+      }
+    })
+
+    return {
+      board,
+      canvas,
+      onMouseMove,
+      onMouseDown,
+      onMouseUp,
+      onMouseLeave,
+      starPosition,
+      cursorStyle,
+      clearCanvas,
+    }
+  },
+})
+</script>
+<style lang="scss" scope>
+#Board {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .canvas {
+    cursor: none;
+  }
+  .cursor {
+    cursor: none;
+    position: absolute;
+    z-index: 1;
+    border: 2px solid $Gray;
+    left: 0;
+    top: 0;
+    pointer-events: none;
+    user-select: none;
+  }
+  .cursor-default {
+    cursor: default;
+  }
+  .cursor-eraser {
+    cursor: none;
+  }
+  .cursor-pen {
+    cursor: none;
+  }
+}
+</style>
