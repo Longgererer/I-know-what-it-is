2ba4b54df2eb50fc3ff8856b52002e63cc6e772e
xiaolong Hao <2072451919@qq.com>
2021-06-08 20:57:15 +0800

    进行websocket连接

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import Router from './router'
 import App from './App.vue'
+import Socket from './utils/socket'
 
 createApp(App).use(Router).mount('#app')
+
+new Socket('ws://127.0.0.1:4041')
