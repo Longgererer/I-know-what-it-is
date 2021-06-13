c4f43cfce98daf65a90833b90e57d0f1b4390626
xiaolong Hao <2072451919@qq.com>
2021-06-13 17:10:06 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import Router from './router'
 import App from './App.vue'
-import Socket from './utils/socket'
+import { buildSocket } from './utils/tools'
 
 createApp(App).use(Router).mount('#app')
 
-new Socket('ws://127.0.0.1:4041')
+buildSocket()
