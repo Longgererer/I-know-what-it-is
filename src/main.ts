cf0b74134a033f75fe1f6f9066234402d1651dc1
xiaolong Hao <2072451919@qq.com>
2021-12-18 16:36:08 +0800

    清空旧代码

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import Router from './router'
-import App from './App.vue'
-import { buildSocket } from './utils/tools'
-
-createApp(App).use(Router).mount('#app')
-
-buildSocket()
--- src/utils/CONFIG.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  WS_URL: 'ws://127.0.0.1:4041'
-}
